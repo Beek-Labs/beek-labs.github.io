dd90f9a338bb0507642559a09307c0038183fee6
BeekLabs <100317722+Beek-Labs@users.noreply.github.com>
2023-01-04 00:14:06 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,39 @@
+document.getElementById('contact_presupuesto').addEventListener('submit', function(event) {
+    // Prevenir la redirección del formulario
+    event.preventDefault();
+  
+    // Crear una instancia de XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+  
+    // Configurar la petición
+    xhr.open('POST', 'https://formbold.com/s/oPZa9');
+  
+    // Enviar la petición
+    xhr.send(new FormData(event.target));
+  });
+  document.getElementById('contact_from').addEventListener('submit', function(event) {
+    // Prevenir la redirección del formulario
+    event.preventDefault();
+  
+    // Crear una instancia de XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+  
+    // Configurar la petición
+    xhr.open('POST', 'https://formbold.com/s/91AE9');
+  
+    // Enviar la petición
+    xhr.send(new FormData(event.target));
+  });
+function show_message(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Email Sent successfully.',
+        text: 'Thanks!',
+        closeOnClickOutside: false,
+        confirmButtonText: 'Exit'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          location.reload();
+        }
+      })
+}
---
+++ js/readmore.js
@@ -0,0 +1,79 @@
+function bscp() {
+    var dots = document.getElementById("dots_1");
+    var moreText = document.getElementById("more_1");
+    var btnText= document.getElementById("BSCP");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = 'inline';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'; 
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-left"></i>'; 
+      moreText.style.display = 'inline';
+    }
+  }
+
+  function cce() {
+    var dots = document.getElementById("dots_2");
+    var moreText = document.getElementById("more_2");
+    var btnText = document.getElementById("CCE");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = 'inline';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'; 
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-left"></i>'; 
+      moreText.style.display = 'inline';
+    }
+  }
+
+  function ccna() {
+    var dots = document.getElementById("dots_3");
+    var moreText = document.getElementById("more_3");
+    var btnText = document.getElementById("CCNA");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = 'inline';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'; 
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-left"></i>'; 
+      moreText.style.display = 'inline';
+    }
+  }
+  
+  function itla() {
+    var dots = document.getElementById("dots_4");
+    var moreText = document.getElementById("more_4");
+    var btnText = document.getElementById("ITLA");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = 'inline';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'; 
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-left"></i>'; 
+      moreText.style.display = 'inline';
+    }
+  }
+
+  function salle() {
+    var dots = document.getElementById("dots_5");
+    var moreText = document.getElementById("more_5");
+    var btnText = document.getElementById("SALLE");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = 'inline';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'; 
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = '<i class="fas fa-long-arrow-alt-left"></i>'; 
+      moreText.style.display = 'inline';
+    }
+  }
