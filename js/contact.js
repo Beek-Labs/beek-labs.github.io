848b69579cdb26f4056518377c50eb3068cb2142
BeekLabs <100317722+Beek-Labs@users.noreply.github.com>
2023-01-08 04:00:04 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -9,6 +9,18 @@ function verificarCaptcha() {
     }
 
     // Si la respuesta es válida, enviar el formulario
-    var formulario = document.getElementById("contact_form");
-    formulario.submit();
+  document.getElementById("contact_from").submit();
+  document.getElementById('contact_from').addEventListener('submit', function(event) {
+    // Prevenir la redirección del formulario
+    event.preventDefault();
+  
+    // Crear una instancia de XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+  
+    // Configurar la petición
+    xhr.open('POST', 'https://formbold.com/s/91AE9');
+  
+    // Enviar la petición
+    xhr.send(new FormData(event.target));
+  });
 }
