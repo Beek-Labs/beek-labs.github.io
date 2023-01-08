284790bb5aaddabdd5847afae8fa6453fba1162c
BeekLabs <100317722+Beek-Labs@users.noreply.github.com>
2023-01-08 03:51:06 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,44 +1,14 @@
-function onSubmit1(token) {
-     document.getElementById("contact_quote").submit();
-document.getElementById('contact_quote').addEventListener('submit', function(event) {
-    // Prevenir la redirección del formulario
-    event.preventDefault();
-  
-    // Crear una instancia de XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-  
-    // Configurar la petición
-    xhr.open('POST', 'https://formbold.com/s/oPZa9');
-  
-    // Enviar la petición
-    xhr.send(new FormData(event.target));
-  });
- }
-function onSubmit2(token) {
-         document.getElementById("contact_from").submit();
-  document.getElementById('contact_from').addEventListener('submit', function(event) {
-    // Prevenir la redirección del formulario
-    event.preventDefault();
-  
-    // Crear una instancia de XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-  
-    // Configurar la petición
-    xhr.open('POST', 'https://formbold.com/s/91AE9');
-  
-    // Enviar la petición
-    xhr.send(new FormData(event.target));
-  });}
-function show_message(){
-    Swal.fire({
-        icon: 'success',
-        title: 'Email Sent successfully.',
-        text: 'Thanks!',
-        closeOnClickOutside: false,
-        confirmButtonText: 'Exit'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          location.reload();
-        }
-      })
+function verificarCaptcha() {
+    // Obtener la respuesta del CAPTCHA
+    var captchaResponse = grecaptcha.getResponse();
+
+    // Si la respuesta es vacía, mostrar un mensaje de error
+    if (captchaResponse.length === 0) {
+        alert("Please solve the CAPTCHA to continue.");
+        return;
+    }
+
+    // Si la respuesta es válida, enviar el formulario
+    var formulario = document.getElementById("contact_form");
+    formulario.submit();
 }
